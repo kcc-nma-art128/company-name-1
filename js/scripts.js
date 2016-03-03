8c2f289368fc081caa1deaa3d60f3d97b91faf3e
Emmanuel Pilande <epilande@gmail.com>
2016-03-03 09:49:03 -1000

    Add click event to nav-toggle
    Show/hide navigation when clicking on nav-toggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+var $navToggle = $('.nav-toggle');
+var $mainNavUl = $('.main-nav ul');
+var $window = $(window);
+var mobileBreakpoint = 767;
+
+$navToggle.on('click', function() {
+  $mainNavUl.toggle();
+  $navToggle.toggleClass('active');
+});
+
+$window.on('resize', function() {
+  if ($window.width() > mobileBreakpoint) {
+    $mainNavUl.show();
+    $navToggle.addClass('active');
+  }
+});
