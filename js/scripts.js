b6112c6022aeed8a1cee10d71201f48bbf391db3
Emmanuel Pilande <epilande@gmail.com>
2016-03-06 09:31:23 -1000

    Wrap jQuery in document ready function

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,18 @@
-var $navToggle = $('.nav-toggle');
-var $mainNavUl = $('.main-nav ul');
-var $window = $(window);
-var mobileBreakpoint = 767;
+$(function() {
+  var $navToggle = $('.nav-toggle');
+  var $mainNavUl = $('.main-nav ul');
+  var $window = $(window);
+  var mobileBreakpoint = 767;
 
-$navToggle.on('click', function() {
-  $mainNavUl.slideToggle();
-  $navToggle.toggleClass('active');
-});
+  $navToggle.on('click', function() {
+    $mainNavUl.slideToggle();
+    $navToggle.toggleClass('active');
+  });
 
-$window.on('resize', function() {
-  if ($window.width() > mobileBreakpoint && $mainNavUl.is(':hidden')) {
-    $mainNavUl.removeAttr('style');
-    $navToggle.removeClass('active');
-  }
+  $window.on('resize', function() {
+    if ($window.width() > mobileBreakpoint && $mainNavUl.is(':hidden')) {
+      $mainNavUl.removeAttr('style');
+      $navToggle.removeClass('active');
+    }
+  });
 });
